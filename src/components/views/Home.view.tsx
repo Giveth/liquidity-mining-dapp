e602772c403c26b368bed0ad04b65cb7dd057ec8
Pourcheriki <pourcheriki@gmail.com>
2021-08-10 01:11:42 +0430

    add Home.view

---
+++ src/components/views/Home.view.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 120px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 132px;
+  letter-spacing: -0.02em;
+  text-align: left;
+`;
+
+const subtitle = styled.h2
+
+function HomeView() {
+  return (
+    <>
+      <Title>Welcome to the <br />
+      Giveth Economy</Title>
+    </>
+  )
+}
+
+
+export default HomeView;
