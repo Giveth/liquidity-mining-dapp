2edb5ab39cc73ec08dd94e3dc8d798e0b7d7703a
Pourcheriki <pourcheriki@gmail.com>
2021-08-10 09:53:09 +0430

    add h1, h2, p components

--- src/components/views/Home.view.tsx
+++ src/components/views/Home.view.tsx
@@ -1,21 +1,68 @@
 import styled from "styled-components";
 
-const Title = styled.h1`
+const H1 = styled.h1`
+  max-width: 989px;
   font-size: 120px;
   font-style: normal;
   font-weight: 700;
   line-height: 132px;
   letter-spacing: -0.02em;
   text-align: left;
+  color: #FFFFFF;
 `;
 
-const subtitle = styled.h2
+const H2 = styled.h2`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 120%;
+  color: #FFFFFF;
+`;
+
+
+const Section = styled.section`
+  min-height: 100vh;
+`;
+
+
+interface PProps {
+  size?: "big"|"medium"|"small";
+}
+
+const P = styled.p<PProps>`
+  font-style: normal;
+  font-weight: normal;
+  font-size: ${ props => {
+    switch (props.size) {
+      case "big":
+        return '32px';
+      case "medium":
+        return '28px';
+      case "small":
+        return '24px';
+      default:
+        return '24px';
+    }
+  }};
+  line-height: 42px;
+  color: #FFFFFF;
+`;
 
 function HomeView() {
   return (
     <>
-      <Title>Welcome to the <br />
-      Giveth Economy</Title>
+      <Section>
+        <H1>Welcome to the Giveth Economy</H1>
+        <P size="big">The Giveth Economy is the collective of projects, donors,
+           builders, and community members building the Future of Giving.</P>
+        <H2>The Economy of Giving</H2>
+        <P>
+        Giveth is building a future in which giving is effortless and people
+         all around the world are rewarded for creating positive change.
+        </P>
+
+      </Section>
+
     </>
   )
 }
