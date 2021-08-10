d32c86abe7ed9b1cd35335e195be987e4c2eb1dc
Pourcheriki <pourcheriki@gmail.com>
2021-08-10 10:47:35 +0430

    add GIVCard

--- src/components/views/Home.view.tsx
+++ src/components/views/Home.view.tsx
@@ -19,8 +19,16 @@ const H2 = styled.h2`
   color: #FFFFFF;
 `;
 
+const H3 = styled.h3`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 44px;
+  line-height: 120%;
+  color: #FFFFFF;
+`;
 
 const Section = styled.section`
+  padding: 102px 0;
   min-height: 100vh;
 `;
 
@@ -60,9 +68,22 @@ const P = styled.p<TypographyProps>`
   color: ${ props => props.color || "#FFFFFF" };
 `;
 
+const Row = styled.div`
+  display:flex;
+`;
+
+const GivCard = styled.div`
+  padding-right: 180px;
+  flex: 1;
+`;
+
+const Container = styled.div`
+  margin: 0 140px;
+`;
+
 function HomeView() {
   return (
-    <>
+    <Container>
       <Section>
         <H1>Welcome to the Giveth Economy</H1>
         <P size="big">The Giveth Economy is the collective of projects, donors,
@@ -72,10 +93,29 @@ function HomeView() {
         Giveth is building a future in which giving is effortless and people
          all around the world are rewarded for creating positive change.
         </P>
+        <Row>
+          <GivCard>
+            <H3>GIV Token</H3>
+            <P>DONATE, EARN, GOVERN</P>
+            <P>
+            GIV fuels and directs the Future of Giving,
+             inspiring people to become Givers and participate in an ecosystem of collective support,
+             abundance, and value-creation.
+            </P>
+          </GivCard>
+          <GivCard>
+            <H3>GIVbacks</H3>
+            <P>GIVE AND RECEIVE</P>
+            <P>
+              GIVbacks is a revolutionary concept that
+               incentives donations to verified projects on Giveth.
+            </P>
+          </GivCard>
+        </Row>
 
       </Section>
 
-    </>
+    </Container>
   )
 }
 
