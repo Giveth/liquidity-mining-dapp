f1d5085dd1dc3e8f85b37469c652b76efa6c5fb0
Pourcheriki <pourcheriki@gmail.com>
2021-08-11 11:55:20 +0430

    add claim route

---
+++ pages/claim.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import ClaimView from '../src/components/views/claim/Claim.view'
+
+export default function HomeRoute() {
+  return (
+    <>
+     <Head>
+        <title>Claim Giv Drop</title>
+     </Head>
+     <ClaimView />
+    </>
+  )
+}
