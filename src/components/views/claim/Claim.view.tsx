13a54c108a0951d5acc3db4f9082752eee3a3f0c
Pourcheriki <pourcheriki@gmail.com>
2021-08-11 11:55:13 +0430

    add steps

---
+++ src/components/views/claim/Claim.view.tsx
@@ -0,0 +1,63 @@
+import { FC, useState } from "react";
+import styled from "styled-components";
+
+const stepsTitle =["Connect", "Invest", "Govern", "Donate", "Claim"]
+
+const Steps = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
+
+interface IStepTitleProps {
+    isActive: boolean;
+}
+
+const StepTitle = styled.div<IStepTitleProps>`
+    width: 104px;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 19px;
+    color: #2FC8E0;
+    opacity: ${props => props.isActive ? 1 : 0.2};
+    position: relative;
+    padding: 8px 0;
+    margin: 4px;
+    ::before {
+        content: "";
+        position: absolute;
+        bottom: -6px;
+        left: 0;
+        width: 100%;
+        height: 6px;
+        border-radius: 3px;
+        background-color: #2FC8E0;
+    }
+`;
+
+interface IStepProps {
+    title: string;
+    isActive: boolean;
+}
+
+const Step: FC<IStepProps> = ({title, isActive}) => {
+    return (
+        <StepTitle isActive={isActive}>{title}</StepTitle>
+    );
+}
+
+const ClaimView = () => {
+    const [step,setStep] = useState(0);
+    return (
+        <div>
+            <Steps>
+                {stepsTitle.map((title, idx)=>
+                    <Step title={title} isActive={step === idx} key={idx} />
+                )}
+            </Steps>
+        </div>
+    );
+}
+
+export default ClaimView;
