c731177fb7042a9df81042d50347697959b2b854
Pourcheriki <pourcheriki@gmail.com>
2021-08-14 01:47:15 +0430

    change direction to flexDirection

--- src/components/styled-components/Grid.tsx
+++ src/components/styled-components/Grid.tsx
@@ -4,12 +4,12 @@ interface IRowProps {
     wrap?: number;
     alignItems?: "stretch"|"center"|"flex-start"|"flex-end"|"baseline";
     justifyContent?: "flex-start"|"flex-end"|"center"|"space-between"|"space-around"|"space-evenly";
-    direction?: "row"|"row-reverse"|"column"|"column-reverse";
+    flexDirection?: "row"|"row-reverse"|"column"|"column-reverse";
 }
 
 export const Row = styled.div<IRowProps>`
   display:flex;
-  direction: ${ props => props.direction ? props.direction : "initial"};
+  flex-direction: ${ props => props.flexDirection ? props.flexDirection : "initial"};
   flex-wrap: ${ props => props.wrap ? "wrap" : "nowrap"};
   align-items:  ${ props => props.alignItems ? props.alignItems : "initial"};
   justify-content:  ${ props => props.justifyContent ? props.justifyContent : "initial"};
