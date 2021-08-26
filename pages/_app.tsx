e071093e0fdbf4af7d8ec97673a347496d8ac62a
Pourcheriki <pourcheriki@gmail.com>
2021-08-26 09:32:52 +0430

    add theme provider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Onboard } from '../src/providers/onboard'
 import { User } from '../src/providers/user'
+import { ThemeProvider } from '../src/providers/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<User>
 			<Onboard>
-				<Component {...pageProps} />
+				<ThemeProvider>
+					<Component {...pageProps} />
+				</ThemeProvider>
 			</Onboard>
 		</User>
 	)
