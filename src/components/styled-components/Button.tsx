3809d96787ff35b6d4d7c07f797aad59a8df0e93
Pourcheriki <pourcheriki@gmail.com>
2021-08-26 11:02:59 +0430

    add button neutral style

--- src/components/styled-components/Button.tsx
+++ src/components/styled-components/Button.tsx
@@ -2,15 +2,17 @@ import styled from 'styled-components'
 
 interface IButtonProps {
 	secondary?: boolean
+	neutral?: boolean
 }
 
 export const Button = styled.button<IButtonProps>`
 	padding: 25px 32px;
-	background: ${props => (props.secondary ? '#E1458D' : '#5326EC')};
+	background: ${props =>
+		props.neutral ? '#F7F7F9' : props.secondary ? '#E1458D' : '#5326EC'};
 	height: 64px;
 	border: 0;
 	border-radius: 88px;
-	color: #ffffff;
+	color: ${props => (props.neutral ? '#092560' : '#ffffff')};
 	width: 100%;
 	font-style: normal;
 	font-weight: bold;
