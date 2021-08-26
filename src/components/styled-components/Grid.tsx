d661bb66635a183bf1fd54697d9d47be3cfddee8
Pourcheriki <pourcheriki@gmail.com>
2021-08-26 09:13:59 +0430

    add general bootstrap container

--- src/components/styled-components/Grid.tsx
+++ src/components/styled-components/Grid.tsx
@@ -22,3 +22,22 @@ export const Row = styled.div<IRowProps>`
 	justify-content: ${props =>
 		props.justifyContent ? props.justifyContent : 'initial'};
 `
+
+export const Container = styled.div`
+	width: 100%;
+	@media (min-width: 576px) {
+		width: 540px;
+	}
+	@media (min-width: 768px) {
+		width: 720px;
+	}
+	@media (min-width: 992px) {
+		width: 960px;
+	}
+	@media (min-width: 1200px) {
+		width: 1140px;
+	}
+	@media (min-width: 1400px) {
+		width: 1320px;
+	}
+`
