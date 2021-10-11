473eba61b3654b715260c4db3ce1c54de1699372
cherik <pourcheriki@gmail.com>
2021-10-11 11:51:44 +0330

    update homv.view component

--- src/components/views/Home.view.tsx
+++ src/components/views/Home.view.tsx
@@ -1,58 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
 import Header from '../Header';
 import TabGIVfarm from '../homeTabs/GIVfarm';
 import TabGIVgarden from '../homeTabs/GIVgarden';
-import TabOverview from '../homeTabs/Overview';
-import { Container } from '../styled-components/Grid';
-import { H1, P } from '../styled-components/Typography';
+import { TabOverviewTop, TabOverviewBottom } from '../homeTabs/Overview';
+
 import Tabs from '../Tabs';
-
-const WelcomeSection = styled.div`
-	background-image: url('/images/Giv.png');
-	background-size: cover;
-	background-repeat: no-repeat;
-	min-height: calc(100vh - 228px);
-	::before {
-		content: url('/images/homebg1.png');
-		position: absolute;
-		top: 48px;
-		left: 0;
-	}
-`;
-
-const Title = styled(H1)`
-	padding-top: 66px;
-	padding-bottom: 42px;
-	max-width: 989px;
-`;
-
-const SubTitle = styled(P)`
-	max-width: 912px;
-	padding-bottom: 288px;
-`;
 
 function HomeView() {
 	return (
 		<>
 			<Header />
-			<WelcomeSection>
-				<Container>
-					<Title>Welcome to the Giveth Economy</Title>
-					<SubTitle size='large'>
-						The Giveth Economy is the collective of projects,
-						donors, builders, and community members building the
-						Future of Giving.
-					</SubTitle>
-				</Container>
-			</WelcomeSection>
+			{/*  */}
 			<Tabs
 				tabs={[
-					{ label: 'Overview', component: <TabOverview /> },
-					{ label: 'GIVgarden', component: <TabGIVgarden /> },
-					{ label: 'GIVfarm', component: <TabGIVfarm /> },
-					{ label: 'GIVbacks', component: <></> },
-					{ label: 'GIVstrem', component: <></> },
+					{ label: 'Overview', topComponent: <TabOverviewTop />,  bottomComponent: <TabOverviewBottom /> },
+					{ label: 'GIVgarden', topComponent: <></>,  bottomComponent: <TabGIVgarden /> },
+					{ label: 'GIVfarm', topComponent: <></>,  bottomComponent: <TabGIVfarm /> },
+					{ label: 'GIVbacks', topComponent: <></>,  bottomComponent: <></> },
+					{ label: 'GIVstrem', topComponent: <></>,  bottomComponent: <></> },
 				]}
 			></Tabs>
 		</>
