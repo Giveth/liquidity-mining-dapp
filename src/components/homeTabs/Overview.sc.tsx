85608f48134787ed1f8807880240b55bd5c3b528
cherik <pourcheriki@gmail.com>
2021-10-16 17:52:43 +0330

    use TopContainer in overview

--- src/components/homeTabs/Overview.sc.tsx
+++ src/components/homeTabs/Overview.sc.tsx
@@ -10,7 +10,7 @@ import {
 	DataBlock,
 	brandColors,
 } from '@giveth/ui-design-system';
-import { TabContainer } from './commons';
+import { TabContainer, TopContainer } from './commons';
 
 export const OverviewBottomContainer = styled.div`
 	background-image: url('/images/backgrounds/curves.svg');
@@ -19,10 +19,7 @@ export const OverviewBottomContainer = styled.div`
 	background-repeat: no-repeat;
 `;
 
-export const OverviewTopContainer = styled.div`
-	background-image: url('/images/Giv.png');
-	background-size: cover;
-`;
+export const OverviewTopContainer = styled(TopContainer)``;
 
 export const OverviewTitle = styled(D3)`
 	font-size: 107px;
