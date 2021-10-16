f5a6477f14627551d50a7bf2b3c5bc5fc8d5333e
cherik <pourcheriki@gmail.com>
2021-10-16 17:52:29 +0330

    add TopContainer

--- src/components/homeTabs/commons.tsx
+++ src/components/homeTabs/commons.tsx
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
 export const TabContainer = styled.div``;
+
+export const TopContainer = styled(TabContainer)`
+	background-image: url('/images/Giv.png');
+	background-size: cover;
+`;
