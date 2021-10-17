50cd63998264c4721b82a38ada164e67b7d18fb0
cherik <pourcheriki@gmail.com>
2021-10-17 12:23:44 +0330

    add TabGardenBottom

--- src/components/homeTabs/GIVgarden.sc.tsx
+++ src/components/homeTabs/GIVgarden.sc.tsx
@@ -13,6 +13,7 @@ import {
 } from '@giveth/ui-design-system';
 import { TopContainer } from './commons';
 import { RewardCard } from '../RewardCard';
+import { Row } from '../styled-components/Grid';
 
 export const GardenTopContainer = styled(TopContainer)`
 	overflow: hidden;
@@ -31,7 +32,7 @@ export const Left = styled.div`
 `;
 
 export const Right = styled.div`
-	width: 426px;
+	width: 360px;
 	position: relative;
 	&::before {
 		content: url('/images/flower1.svg');
@@ -65,3 +66,73 @@ export const GardenRewardCard = styled(RewardCard)`
 `;
 
 export const GardenBottomContainer = styled.div``;
+
+export const Section1Title = styled(H3)`
+	margin-top: 80px;
+	margin-bottom: 24px;
+`;
+
+export const Section1Subtitle = styled(QuoteText)`
+	width: 802px;
+`;
+
+export const OpenGardenButton = styled(Button)`
+	width: 251px;
+	padding: 24px 55px;
+`;
+
+export const Section2Title = styled(H1)`
+	color: ${brandColors.giv[200]};
+	margin-top: 120px;
+	margin-bottom: 40px;
+`;
+
+export const GovernanceDB = styled(DataBlock)`
+	width: 343px;
+`;
+
+export const GovernanceBtn = styled(Button)`
+	color: ${brandColors.cyan[500]};
+	padding: 16px 0;
+`;
+
+export const GovernanceRaw = styled(Row)`
+	margin-bottom: 120px;
+`;
+
+export const VoteCard = styled.div`
+	background-color: #3c14c5;
+	padding: 107px 103px 140px 231px;
+	background-image: url('/images/flower4.svg'),
+		url('/images/backgrounds/giv-outline.svg');
+	background-repeat: no-repeat, repeat;
+	background-position: bottom left, top;
+	min-height: 480px;
+	margin: 80px 0 45px;
+	position: relative;
+	::before {
+		content: url('/images/bee1.svg');
+		position: absolute;
+		bottom: 30px;
+		right: 20px;
+	}
+	::after {
+		content: url('/images/bee2.svg');
+		position: absolute;
+		top: 30px;
+		left: 40px;
+	}
+`;
+
+export const VoteCardDesc = styled(QuoteText)`
+	width: 754px;
+	color: ${brandColors.giv[200]};
+	margin-top: 24px;
+	margin-bottom: 8px;
+`;
+
+export const VoteCardButton = styled(Button)`
+	display: block;
+	padding: 24px 73px;
+	margin-left: auto;
+`;
--- src/components/homeTabs/GIVgarden.tsx
+++ src/components/homeTabs/GIVgarden.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import {
-	Container,
-	D1,
-	IconGIVGarden,
-	QuoteText,
-} from '@giveth/ui-design-system';
+import { H1, Container, IconGIVGarden } from '@giveth/ui-design-system';
 
 import { Row } from '../styled-components/Grid';
 import {
@@ -16,7 +11,18 @@ import {
 	Subtitle,
 	Right,
 	GardenRewardCard,
+	GovernanceBtn,
+	GovernanceDB,
+	OpenGardenButton,
+	Section1Subtitle,
+	Section1Title,
+	Section2Title,
+	GovernanceRaw,
+	VoteCard,
+	VoteCardDesc,
+	VoteCardButton,
 } from './GIVgarden.sc';
+import styled from 'styled-components';
 
 export const TabGardenTop = () => {
 	return (
@@ -29,8 +35,9 @@ export const TabGardenTop = () => {
 							<IconGIVGarden size={64} />
 						</Row>
 						<Subtitle size='medium'>
-							The GIVgarden is the decentralized governance
-							platform for the GIVeconomy.
+							The Giveth Economy is the collective of projects,
+							donors, builders, and community members building the
+							Future of Giving.
 						</Subtitle>
 					</Left>
 					<Right>
@@ -49,7 +56,72 @@ export const TabGardenTop = () => {
 export const TabGardenBottom = () => {
 	return (
 		<GardenBottomContainer>
-			<Container></Container>
+			<Container>
+				<Section1Title weight={700}>Vote & Earn</Section1Title>
+				<Row justifyContent='space-between' alignItems='flex-start'>
+					<Section1Subtitle size='small'>
+						GIV token holders control the treasury, roadmap and
+						mission of the Giveth ecosystem. By voting in the
+						GIVgarden you earn rewards on your staked GIV!
+					</Section1Subtitle>
+					<OpenGardenButton
+						buttonType='primary'
+						label='OPEN GARDENS'
+						size='large'
+					/>
+				</Row>
+				<Section2Title weight={500}>
+					Three Pillars of Governance
+				</Section2Title>
+				<GovernanceRaw justifyContent='space-between'>
+					<GovernanceDB
+						title='Covenant'
+						button={
+							<GovernanceBtn
+								label='LEARN MORE'
+								buttonType='texty'
+							/>
+						}
+					>
+						A decentralized social contract that outlines standards
+						for on-chain and off-chain community behaviour.
+					</GovernanceDB>
+					<GovernanceDB
+						title='Conviction Voting'
+						button={
+							<GovernanceBtn
+								label='LEARN MORE'
+								buttonType='texty'
+							/>
+						}
+					>
+						A token-weighted decision making tool, used for funds
+						allocation, in which voting power is accrued as a
+						function of tokens staked and time.
+					</GovernanceDB>
+					<GovernanceDB
+						title='Tao Voting'
+						button={
+							<GovernanceBtn
+								label='LEARN MORE'
+								buttonType='texty'
+							/>
+						}
+					>
+						A token-weighted YES/NO decision making tool, with the
+						option of delegation, that is used to make non-financial
+						decisions in the GIVgarden.
+					</GovernanceDB>
+				</GovernanceRaw>
+				<VoteCard>
+					<H1>Vote in the GIVgarden</H1>
+					<VoteCardDesc size='small'>
+						The GIVgarden empowers the Giveth community to
+						coordinate around shared resources from the bottom up.
+					</VoteCardDesc>
+					<VoteCardButton label='OPEN GARDEN' buttonType='primary' />
+				</VoteCard>
+			</Container>
 		</GardenBottomContainer>
 	);
 };
