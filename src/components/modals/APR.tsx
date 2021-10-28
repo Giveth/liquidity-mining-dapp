66b901122ea423c6432e79b39cd73c6b19cb7335
cherik <pourcheriki@gmail.com>
2021-10-28 13:56:41 +0330

    remove APRModal Unused Import

--- src/components/modals/APR.tsx
+++ src/components/modals/APR.tsx
@@ -1,24 +1,5 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Modal, IModal } from './Modal';
-import Lottie from 'react-lottie';
-import LoadingAnimation from '../../animations/loading.json';
-import CheckAnimation from '../../animations/check.json';
-import {
-	brandColors,
-	neutralColors,
-	Button,
-	Caption,
-	IconGiveth,
-	IconGIVStream,
-	IconHelp,
-	Lead,
-	P,
-	Title,
-	H6,
-	GLink,
-	OulineButton,
-} from '@giveth/ui-design-system';
-import { Row } from '../styled-components/Grid';
 import styled from 'styled-components';
 interface IHarvesModalProps extends IModal {}
 
@@ -27,7 +8,7 @@ export const APRModal: FC<IHarvesModalProps> = ({
 	setShowModal,
 }) => {
 	return (
-		<Modal showModal={showModal} setShowModal={setShowModal} title='APR'>
+		<Modal showModal={showModal} setShowModal={setShowModal}>
 			<APRModalContainer>Hi</APRModalContainer>
 		</Modal>
 	);
