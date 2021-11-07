e43c34b35326b71444468f4d87ca6538f31f9094
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-11-07 13:10:27 -0300

    feat: preview card example

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,11 @@
-import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import Document, {
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext,
+} from 'next/document';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
@@ -26,5 +32,51 @@ export default class MyDocument extends Document {
 		} finally {
 			sheet.seal();
 		}
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta name='title' content='Giveth Economy' />
+					<meta
+						name='description'
+						content='The GIVeconomy empowers our collective to build the Future of Giving.'
+					/>
+
+					<meta property='og:type' content='website' />
+					<meta property='og:url' content='http://giveth.io/' />
+					<meta property='og:title' content='Giveth Economy' />
+					<meta
+						property='og:description'
+						content='The GIVeconomy empowers our collective to build the Future of Giving.'
+					/>
+					<meta
+						property='og:image'
+						content='https://i.ibb.co/HTbdCdd/Thumbnail.png'
+					/>
+
+					<meta
+						property='twitter:card'
+						content='summary_large_image'
+					/>
+					<meta property='twitter:url' content='http://giveth.io/' />
+					<meta property='twitter:title' content='Giveth Economy' />
+					<meta
+						property='twitter:description'
+						content='The GIVeconomy empowers our collective to build the Future of Giving.'
+					/>
+					<meta
+						property='twitter:image'
+						content='https://i.ibb.co/HTbdCdd/Thumbnail.png'
+					/>
+					<meta name='twitter:card' content='summary_large_image' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
