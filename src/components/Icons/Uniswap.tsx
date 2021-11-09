72967df2f48d46bbe78cc78e7ff17a7b35dc8a26
cherik <pourcheriki@gmail.com>
2021-11-09 11:37:53 +0330

    add IconUniswap

---
+++ src/components/Icons/Uniswap.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import Image from 'next/image';
+import { ICurrencyIconProps } from './type';
+
+export const IconUniswap: FC<ICurrencyIconProps> = ({ size = 16 }) => {
+	return (
+		<Image
+			src={`/images/currencies/uniswap/${size}.svg`}
+			alt='uniswap'
+			width={size}
+			height={size}
+		/>
+	);
+};
