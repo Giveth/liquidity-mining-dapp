746d3b9bad58e5e674f9697607f4af5cefa1ccd5
cherik <pourcheriki@gmail.com>
2021-11-09 11:37:40 +0330

    add IconHoneyswap

---
+++ src/components/Icons/Honeyswap.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import Image from 'next/image';
+import { ICurrencyIconProps } from './type';
+
+export const IconHoneyswap: FC<ICurrencyIconProps> = ({ size = 16 }) => {
+	return (
+		<Image
+			src={`/images/currencies/honeyswap/${size}.svg`}
+			alt='Honeyswap'
+			width={size}
+			height={size}
+		/>
+	);
+};
