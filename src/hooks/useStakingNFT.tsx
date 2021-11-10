0a4572c8f33df76bd357a3d2b622a8e9fcf6f211
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-11-09 23:15:43 -0300

    fix: typo on network checking

--- src/hooks/useStakingNFT.tsx
+++ src/hooks/useStakingNFT.tsx
@@ -43,7 +43,7 @@ export const useStakingNFT = () => {
 			!rewardToken ||
 			!poolAddress ||
 			!incentiveRefundeeAddress ||
-			network === config.MAINNET_NETWORK_NUMBER
+			!(network === config.MAINNET_NETWORK_NUMBER)
 		)
 			return { key: null };
 
