fee55239c4ff4a6b1d503d09bb159ebe3528bc4b
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-11-09 22:22:52 -0300

    refact: remove contexts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,16 @@ import { OnboardProvider } from '@/context/onboard.context';
 import { UserProvider } from '@/context/user.context';
 import { ThemeProvider } from '@/context/theme.context';
 import { TokenBalanceProvider } from '@/context/tokenBalance.context';
-import { ContractsProvider } from '@/context/useContracts';
-import { ERC721NftsProvider } from '@/context/useV3Liquidity';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<UserProvider>
 			<OnboardProvider>
-				<ContractsProvider>
-					<ERC721NftsProvider>
-						<TokenBalanceProvider>
-							<ThemeProvider>
-								<Component {...pageProps} />
-							</ThemeProvider>
-						</TokenBalanceProvider>
-					</ERC721NftsProvider>
-				</ContractsProvider>
+				<TokenBalanceProvider>
+					<ThemeProvider>
+						<Component {...pageProps} />
+					</ThemeProvider>
+				</TokenBalanceProvider>
 			</OnboardProvider>
 		</UserProvider>
 	);
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,6 +1,4 @@
-import { useV3Liquidity } from './useV3Liquidity';
-import { useContracts } from './useContracts';
 import { useOnboard } from './onboard.context';
 import { useTokenBalance } from './tokenBalance.context';
 
-export { useV3Liquidity, useContracts, useOnboard, useTokenBalance };
+export { useOnboard, useTokenBalance };
