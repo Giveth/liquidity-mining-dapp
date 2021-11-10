d27d7c194b1651221b4c8507a59438ce4358747c
cherik <pourcheriki@gmail.com>
2021-11-10 14:19:40 +0330

    update changeNetwork Modal

--- src/components/modals/ChangeNetwork.tsx
+++ src/components/modals/ChangeNetwork.tsx
@@ -1,6 +1,11 @@
-import { FC } from 'react';
+import { FC, useContext, useEffect } from 'react';
 import { Modal, IModal } from './Modal';
 import styled from 'styled-components';
+import config from '../../configuration';
+import { IconEthereum } from '../Icons/Eth';
+import { IconXDAI } from '../Icons/XDAI';
+import { H4, B, neutralColors } from '@giveth/ui-design-system';
+import { OnboardContext } from '../../context/onboard.context';
 interface IChangeNetworkModalProps extends IModal {
 	targetNetwork: number;
 }
@@ -10,13 +15,40 @@ export const ChangeNetworkModal: FC<IChangeNetworkModalProps> = ({
 	setShowModal,
 	targetNetwork,
 }) => {
+	const { network: walletNetwork } = useContext(OnboardContext);
+
+	useEffect(() => {
+		if (walletNetwork === targetNetwork) {
+			setShowModal(false);
+		}
+	}, [walletNetwork, setShowModal, targetNetwork]);
+
+	const NetworkName =
+		targetNetwork === config.MAINNET_NETWORK_NUMBER ? 'Ethereum' : 'xDAI';
+
 	return (
 		<Modal showModal={showModal} setShowModal={setShowModal}>
-			<APRModalContainer>Hi</APRModalContainer>
+			<ChangeNetworkModalContainer>
+				{targetNetwork === config.MAINNET_NETWORK_NUMBER ? (
+					<IconEthereum size={64} />
+				) : (
+					<IconXDAI size={64} />
+				)}
+				<Title>Switch to {NetworkName}</Title>
+				<Desc>Please switch your wallet network to {NetworkName}.</Desc>
+			</ChangeNetworkModalContainer>
 		</Modal>
 	);
 };
 
-const APRModalContainer = styled.div`
-	width: 370px;
+const ChangeNetworkModalContainer = styled.div`
+	width: 500px;
+	padding: 62px 60px;
+	color: ${neutralColors.gray[100]};
 `;
+
+const Title = styled(H4)`
+	margin: 18px 0 24px;
+`;
+
+const Desc = styled(B)``;
