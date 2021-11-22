7c0a0fd91ede4ce5a674026bf7dfce3cd85bfc2c
cherik <pourcheriki@gmail.com>
2021-11-22 10:42:24 +0330

    add convertMSToHRD

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -12,3 +12,14 @@ export const calcStream = (start: string, end: string, cliff: string) => {
 		return Zero;
 	}
 };
+
+// HRM: Human Readable Date.
+export const convertMSToHRD = (ms: number) => {
+	let rem = ms / 1000;
+	const y = Math.floor(rem / 31536000);
+	rem -= y * 31536000;
+	const m = Math.floor(rem / 2592000);
+	rem -= m * 2592000;
+	const d = Math.floor(rem / 86400);
+	return { y, m, d };
+};
