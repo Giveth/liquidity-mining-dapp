5e551f612cbaaf6f7f645f89f1d23bc3c52a2da7
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-12-10 18:39:50 -0300

    fix: donate math

--- src/components/cards/Donate.tsx
+++ src/components/cards/Donate.tsx
@@ -1,10 +1,10 @@
-import { useState, ChangeEvent, FC, useContext } from 'react';
+import { useState, ChangeEvent, FC, useContext, useEffect } from 'react';
 import Image from 'next/image';
 import styled from 'styled-components';
 import { InputWithUnit } from '../input';
 import { Row } from '../styled-components/Grid';
 import { H2, H4, P } from '../styled-components/Typography';
-import { ArrowButton, Card, Header, ICardProps, MaxGIV } from './common';
+import { ArrowButton, Card, ICardProps, MaxGIV } from './common';
 import {
 	ClaimViewContext,
 	IClaimViewCardProps,
@@ -23,6 +23,10 @@ const DonateCardContainer = styled(Card)`
 		right: 0;
 		z-index: 0;
 	}
+`;
+
+const Header = styled.div`
+	margin-bottom: 60px;
 `;
 
 const Title = styled(H2)`
@@ -62,6 +66,45 @@ const DonateGIVEarn = styled.div`
 	text-align: left;
 `;
 
+const PoolCardContainer = styled.div`
+	z-index: 1;
+`;
+
+const PoolCardTitle = styled.div`
+	font-size: 16px;
+	padding-bottom: 12px;
+`;
+
+const PoolCard = styled.div`
+	width: 350px;
+	height: 164px;
+	padding: 10px 30px;
+
+	background: #211985;
+	border-radius: 16px;
+	z-index: 1;
+`;
+
+const PoolItems = styled.div`
+	padding: 12px 0;
+`;
+
+const PoolItem = styled.div`
+	font-size: 14px;
+	height: 40px;
+	line-height: 40px;
+	display: flex;
+	gap: 6px;
+`;
+
+const PoolItemBold = styled.div`
+	font-size: 16px;
+	font-weight: 500;
+	line-height: 40px;
+	display: flex;
+	gap: 6px;
+`;
+
 const DonateFooter = styled.div`
 	max-width: 500px;
 	font-size: 12px;
@@ -73,6 +116,8 @@ export const DonateCard: FC<IClaimViewCardProps> = ({ index }) => {
 	const { claimableAmount } = useContext(UserContext);
 
 	const [donation, setDonation] = useState(0);
+	const [potentialClaim, setPotentialClaim] = useState<number>(0);
+	const [earnEstimate, setEarnEstimate] = useState<number>(0);
 
 	const stackedChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		if (e.target.value.length === 0) {
@@ -84,16 +129,23 @@ export const DonateCard: FC<IClaimViewCardProps> = ({ index }) => {
 		}
 	};
 
+	useEffect(() => {
+		const donationWithGivBacks = donation * 0.75;
+		setPotentialClaim(donationWithGivBacks * 0.1);
+		setEarnEstimate((donationWithGivBacks * 0.9) / (52 * 5));
+	}, [donation]);
+
 	return (
 		<DonateCardContainer activeIndex={activeIndex} index={index}>
 			<Header>
 				<Title as='h1'>How to use your GIV</Title>
 				<Desc size='small' color={'#CABAFF'}>
-					Donate to verified projects on Giveth to earn GIV with
-					GIVbacks.
+					Donate to verified projects to get GIV with GIVbacks. The
+					project gets 100% of your donation, and you get GIV back
+					from Giveth!
 				</Desc>
 			</Header>
-			<Row alignItems={'center'} justifyContent={'flex-start'}>
+			<Row alignItems={'center'} justifyContent={'space-between'}>
 				<DonateRow
 					flexDirection='column'
 					justifyContent='space-between'
@@ -118,7 +170,7 @@ export const DonateCard: FC<IClaimViewCardProps> = ({ index }) => {
 						</DonateInput>
 					</div>
 				</DonateRow>
-				<GetBack flexDirection='column' justifyContent='space-between'>
+				{/* <GetBack flexDirection='column' justifyContent='space-between'>
 					<H4 as='h2'>You can get back up to</H4>
 					<Row alignItems={'center'} justifyContent={'space-between'}>
 						<DonateGIVEarn>{donation}</DonateGIVEarn>
@@ -132,7 +184,40 @@ export const DonateCard: FC<IClaimViewCardProps> = ({ index }) => {
 							/>
 						</DonateLabel>
 					</Row>
-				</GetBack>
+				</GetBack> */}
+				<PoolCardContainer>
+					<PoolCardTitle>If you stake for 1 month:</PoolCardTitle>
+					<PoolCard>
+						<PoolItems>
+							<Row justifyContent='space-between'>
+								<PoolItem>GIVbacks</PoolItem>
+								<PoolItemBold>
+									<Image
+										src='/images/icons/star.svg'
+										height='16'
+										width='16'
+										alt='Star icon'
+									/>
+									75%
+								</PoolItemBold>
+							</Row>
+							<Row justifyContent='space-between'>
+								<PoolItem>Claimable</PoolItem>
+								<PoolItemBold>
+									{/* {formatEthHelper(, 2)} GIV */}
+									{potentialClaim} GIV
+								</PoolItemBold>
+							</Row>
+							<Row justifyContent='space-between'>
+								<PoolItem>Streaming</PoolItem>
+								<PoolItemBold>
+									{/* {earnEstimate.toFixed(2)} GIV/week */}
+									{earnEstimate.toFixed(2)} GIV/week
+								</PoolItemBold>
+							</Row>
+						</PoolItems>
+					</PoolCard>
+				</PoolCardContainer>
 			</Row>
 			<Row>
 				<DonateFooter>
