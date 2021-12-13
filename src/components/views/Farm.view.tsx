4baa9388e32c7aa92a685032d6ce218f91feb31a
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-12-13 20:43:49 -0300

    fix: remove network modal

--- src/components/views/Farm.view.tsx
+++ src/components/views/Farm.view.tsx
@@ -1,27 +1,12 @@
-import config from '@/configuration';
-import { useOnboard } from '@/context';
 import { Footer } from '../Footer';
 import Header from '../Header';
 import { TabGIVfarmTop, TabGIVfarmBottom } from '../homeTabs/GIVfarm';
-import { WrongNetworkInnerModal } from '../modals/WrongNetwork';
 
 import Tabs from '../Tabs';
 
 function GIVfarmView() {
-	const { isReady, network } = useOnboard();
-	const { MAINNET_NETWORK_NUMBER, XDAI_NETWORK_NUMBER } = config;
-	const targetNetworks = [MAINNET_NETWORK_NUMBER, XDAI_NETWORK_NUMBER];
-
 	return (
 		<>
-			{isReady &&
-				network !== MAINNET_NETWORK_NUMBER &&
-				network !== XDAI_NETWORK_NUMBER && (
-					<WrongNetworkInnerModal
-						targetNetworks={targetNetworks}
-						text='GIVfarm is available on Mainnet and xDAI networks.'
-					/>
-				)}
 			<Header />
 			<TabGIVfarmTop />
 			<Tabs />
