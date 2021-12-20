cbad462fa283ed2329df9014ed2b771a1245e8b0
cherik <pourcheriki@gmail.com>
2021-12-20 15:49:32 +0330

    add DurationToString

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -13,6 +13,21 @@ export const DurationToYMDhms = (ms: number) => {
 	return { Y: Y, M, D, h, m, s };
 };
 
+export const DurationToString = (ms: number, length: number = 3) => {
+	let i = 0;
+	const temp: { [key: string]: number } = DurationToYMDhms(ms);
+	const res: string[] = [];
+	for (const key in temp) {
+		if (Object.prototype.hasOwnProperty.call(temp, key)) {
+			const value: number = temp[key];
+			if (value) {
+				res.push(`${value}${key}`);
+			}
+		}
+	}
+	return res.slice(0, length).join(' ');
+};
+
 export const formatDate = (date: Date) => {
 	return date
 		.toLocaleString('en-US', {
