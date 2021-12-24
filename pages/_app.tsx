d3d74cdaa47ffeb57b96bf429563cc7795c375f5
cherik <pourcheriki@gmail.com>
2021-12-24 16:48:35 +0330

    use ComingSoon in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,34 +11,44 @@ import { TokenDistroProvider } from '@/context/tokenDistro.context';
 
 import { MobileModal } from '@/components/modals/Mobile';
 import { useEffect, useState } from 'react';
+import { ComingSoon } from '@/components/modals/ComingSoon';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [showMobileModal, setShowMobileModal] = useState(false);
+	const [showcommingSoon, setShowcommingSoon] = useState(true);
 
 	useEffect(() => {
 		if (window.screen.width < 640) setShowMobileModal(true);
 	}, []);
 
 	return (
-		<OnboardProvider>
-			<BalanceProvider>
-				<TokenDistroProvider>
-					<NftsProvider>
-						<FarmProvider>
-							<ThemeProvider>
-								<Component {...pageProps} />
-								{showMobileModal && (
-									<MobileModal
-										showModal={showMobileModal}
-										setShowModal={setShowMobileModal}
-									/>
-								)}
-							</ThemeProvider>
-						</FarmProvider>
-					</NftsProvider>
-				</TokenDistroProvider>
-			</BalanceProvider>
-		</OnboardProvider>
+		<>
+			<OnboardProvider>
+				<BalanceProvider>
+					<TokenDistroProvider>
+						<NftsProvider>
+							<FarmProvider>
+								<ThemeProvider>
+									<Component {...pageProps} />
+									{showMobileModal && (
+										<MobileModal
+											showModal={showMobileModal}
+											setShowModal={setShowMobileModal}
+										/>
+									)}
+								</ThemeProvider>
+							</FarmProvider>
+						</NftsProvider>
+					</TokenDistroProvider>
+				</BalanceProvider>
+			</OnboardProvider>
+			{showcommingSoon && (
+				<ComingSoon
+					showModal={showcommingSoon}
+					setShowModal={setShowcommingSoon}
+				/>
+			)}
+		</>
 	);
 }
 export default MyApp;
