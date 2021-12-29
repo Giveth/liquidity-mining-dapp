3090d6ca7de0060801c412cb23dcd248872e6006
cherik <pourcheriki@gmail.com>
2021-12-29 20:53:24 +0330

    add rows

--- src/components/menu/RewardMenu.tsx
+++ src/components/menu/RewardMenu.tsx
@@ -1,9 +1,10 @@
 import { OnboardContext } from '@/context/onboard.context';
-import { Overline, B, GLink, brandColors } from '@giveth/ui-design-system';
+import { Overline, P, B, GLink, brandColors, Caption } from '@giveth/ui-design-system';
 import { useContext } from 'react';
 import styled from 'styled-components';
 import { Row } from '../styled-components/Grid';
 import { RewardMenuContainer } from './RewardMenu.sc';
+import Image from 'next/image';
 
 export const RewardMenu = () => {
 	const { network, connect, address, provider } = useContext(OnboardContext);
@@ -15,11 +16,25 @@ export const RewardMenu = () => {
 				<B>{provider?._network?.name}</B>
 				<SwithNetwork>Switch network</SwithNetwork>
 			</NetworkRow>
-			<div>Item 1</div>
-			<div>Item 2</div>
-			<div>Item 3</div>
-			<div>Item 4</div>
-			<div>Item 5</div>
+			<FlowrateBox>
+				<Overline styleType='Small'>GIVStream Flowrate</Overline>
+				<FlowrateRow>
+					<Image
+						src='/images/icons/thunder.svg'
+						height='16'
+						width='12'
+						alt='Thunder image'
+					/>
+					<FlowrateAmount>{9.588}</FlowrateAmount>
+					<FlowrateUnit>GIV/week</FlowrateUnit>
+				</FlowrateRow>
+			</FlowrateBox>
+			<PartRow>
+				<PartInfo>
+					<PartTitle>From Givstream</PartTitle>
+					<PartAmount></PartAmount>
+				</PartInfo>
+			</PartRow>
 		</RewardMenuContainer>
 	);
 };
@@ -32,3 +47,34 @@ export const NetworkRow = styled(Row)`
 export const SwithNetwork = styled(GLink)`
 	color: ${brandColors.pinky[500]};
 `;
+
+export const FlowrateBox = styled.div`
+	background-color: ${brandColors.giv[500]};
+	margin: 16px -16px;
+	border-radius: 8px;
+	padding: 8px 16px;
+`;
+
+export const FlowrateRow = styled(Row)`
+	align-items: center;
+	margin-top: 12px;
+`;
+
+export const FlowrateAmount = styled(P)`
+	padding-left: 2px;
+`;
+
+export const FlowrateUnit = styled(P)`
+	padding-left: 2px;
+`;
+
+export const PartRow = styled(Row)`
+	justify-content: space-between;
+`;
+
+export const PartInfo = styled.div``;
+
+export const PartTitle = styled(Overline)`
+	margin-bottom: 10px;
+`;
+export const PartAmount = styled(Caption)``;
