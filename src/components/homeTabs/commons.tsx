e08a9f226df41d9389ed074335cd33aafc124f4b
cherik <pourcheriki@gmail.com>
2022-01-10 18:59:45 +0330

    add TopInnerContainer

--- src/components/homeTabs/commons.tsx
+++ src/components/homeTabs/commons.tsx
@@ -1,10 +1,17 @@
-import { brandColors, GLink } from '@giveth/ui-design-system';
+import { brandColors, Container, GLink } from '@giveth/ui-design-system';
 import styled from 'styled-components';
+import { Row } from '../styled-components/Grid';
 
 export const TabContainer = styled.div``;
 
 export const TopContainer = styled(TabContainer)`
 	background-image: url('/images/backgrounds/givup.svg');
+	height: 370px;
+`;
+
+export const TopInnerContainer = styled(Container)`
+	display: flex;
+	flex-direction: column;
 	height: 370px;
 `;
 
